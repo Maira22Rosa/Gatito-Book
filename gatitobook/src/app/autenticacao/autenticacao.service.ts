7e6ded27dd719da3fe59e94a32fcdd61b481ec04
MairaRosa <maira.rosa@simples.software>
2021-12-10 14:58:02 -0300

    added photo listing

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -3,10 +3,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UsuarioService } from './usuario/usuario.service';
 import { tap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
 
-
-const API = environment.apiURL;
 @Injectable({
   providedIn: 'root',
 })
@@ -19,7 +16,7 @@ export class AutenticacaoService {
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this.httpClient
       .post(
-        `${API}/user/login`,
+        'http://localhost:3000/user/login',
         {
           userName: usuario,
           password: senha,
